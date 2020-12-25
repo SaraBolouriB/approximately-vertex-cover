bd8481d40220e6937d8557e0aca19e134b7953f8
SaraBolouriB <boloorisara@gmail.com>
2020-12-25 17:14:28 +0330

    updated main.py

--- source/main.py
+++ source/main.py
@@ -8,9 +8,12 @@ if __name__ == "__main__":
     
     init = 0
     graph = generate_random_graph(node=node, edge=edge)
+
     start_time = time.time()
     VC = compute_vertex_cover(init=init , graph=graph)
     end_time = time.time()
+
+    ## PRINT OUTPUTS ----------------------------------------------
     print("The Vertex Cover: ", VC)
     print("Number Of Vertices:", len(VC))
     print("Time: ",end_time - start_time)
